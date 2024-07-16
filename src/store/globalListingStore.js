0246fbe8a5d9844683ea019ecf3899dab35eef7a
ozzy <iturturika89@gmail.com>
2024-07-16 15:35:06 +0300

    inti3

--- src/store/globalListingStore.js
+++ src/store/globalListingStore.js
@@ -1,41 +1,44 @@
 // stores/listingStore.js
-import create from 'zustand';
+import { create } from 'zustand';
 import axios from 'axios';
 
 const useListingStore = create((set) => ({
-    listings: [],
-    fetchListings: async () => {
-        try {
-            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/client/v1/listing/get_listings`, {
-                "-": {
-                    "UserID": "00000000-0000-0000-0000-000000000000",
-                    "DeviceType": 0,
-                    "FirebaseToken": "",
-                    "Email": "",
-                    "Locale": "",
-                    "IsProSeller": false,
-                    "Verified": false,
-                    "UserAuth": false
-                },
-                "detailed": true,
-                "pagination": {
-                    "limit": 3,
-                    "page": 0
-                },
-                "sort": {
-                    "key": "",
-                    "type": ""
-                },
-                "filters": {
-                    "price_max": null,
-                    "price_min": null
-                }
-            });
-            set({ listings: response.data });
-        } catch (error) {
-            console.error('Failed to fetch listings', error);
-        }
-    },
+  listings: [],
+  fetchListings: async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/client/v1/listing/get_listings`,
+        {
+          '-': {
+            UserID: '00000000-0000-0000-0000-000000000000',
+            DeviceType: 0,
+            FirebaseToken: '',
+            Email: '',
+            Locale: '',
+            IsProSeller: false,
+            Verified: false,
+            UserAuth: false,
+          },
+          detailed: true,
+          pagination: {
+            limit: 3,
+            page: 0,
+          },
+          sort: {
+            key: '',
+            type: '',
+          },
+          filters: {
+            price_max: null,
+            price_min: null,
+          },
+        },
+      );
+      set({ listings: response.data });
+    } catch (error) {
+      console.error('Failed to fetch listings', error);
+    }
+  },
 }));
 
 export default useListingStore;
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,43 +1,46 @@
-import create from 'zustand';
-import axios from "axios";
+import { create } from 'zustand';
+import axios from 'axios';
 
 const useUserStore = create((set) => ({
-    user: null,
-    accessToken: localStorage.getItem('access_token'),
-    refreshToken: localStorage.getItem('refresh_token'),
-    authorized: false,
-    setUser: (user, accessToken, refreshToken) => {
-        localStorage.setItem('access_token', accessToken);
-        localStorage.setItem('refresh_token', refreshToken);
-        set({ user, accessToken, refreshToken, authorized: true });
-    },
-    setAccessToken: (accessToken) => {
-        localStorage.setItem('access_token', accessToken);
-        set({ accessToken });
-    },
-    logout: () => {
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('refresh_token');
-        set({ user: null, accessToken: null, refreshToken: null, authorized: false });
-    },
-    refreshAccessToken: async () => {
-        const refreshToken = localStorage.getItem('refresh_token');
-        if (!refreshToken) return null;
-        try {
-            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/client/v1/user/refresh`, { refresh_token: refreshToken });
-            const { access_token, refresh_token } = response.data;
-            localStorage.setItem('access_token', access_token);
-            localStorage.setItem('refresh_token', refresh_token);
-            set({ accessToken: access_token, refreshToken: refresh_token });
-            return access_token;
-        } catch (error) {
-            console.error('Failed to refresh access token:', error);
-            localStorage.removeItem('access_token');
-            localStorage.removeItem('refresh_token');
-            set({ user: null, accessToken: null, refreshToken: null, authorized: false });
-            return null;
-        }
+  user: null,
+  accessToken: localStorage.getItem('access_token'),
+  refreshToken: localStorage.getItem('refresh_token'),
+  authorized: false,
+  setUser: (user, accessToken, refreshToken) => {
+    localStorage.setItem('access_token', accessToken);
+    localStorage.setItem('refresh_token', refreshToken);
+    set({ user, accessToken, refreshToken, authorized: true });
+  },
+  setAccessToken: (accessToken) => {
+    localStorage.setItem('access_token', accessToken);
+    set({ accessToken });
+  },
+  logout: () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    set({ user: null, accessToken: null, refreshToken: null, authorized: false });
+  },
+  refreshAccessToken: async () => {
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (!refreshToken) return null;
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/client/v1/user/refresh`,
+        { refresh_token: refreshToken },
+      );
+      const { access_token, refresh_token } = response.data;
+      localStorage.setItem('access_token', access_token);
+      localStorage.setItem('refresh_token', refresh_token);
+      set({ accessToken: access_token, refreshToken: refresh_token });
+      return access_token;
+    } catch (error) {
+      console.error('Failed to refresh access token:', error);
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+      set({ user: null, accessToken: null, refreshToken: null, authorized: false });
+      return null;
     }
+  },
 }));
 
 export default useUserStore;
